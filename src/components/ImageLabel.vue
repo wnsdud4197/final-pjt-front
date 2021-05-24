1e80bbed1d9ea8a9c973e642f6f68a8c5d097b4f
wnsdud4197 <wnsdud4197@naver.com>
2021-05-24 15:22:25 +0900

    Add feature imageRecommend

---
+++ src/components/ImageLabel.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{ label.label }}, {{ label.score }}</p>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageLabel',
+  props: {
+    label: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
