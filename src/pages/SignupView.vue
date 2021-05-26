ae23ec70a4b56820eab4613c38b286ce6320d944
eunzi-kim <rladmswlek@naver.com>
2021-05-26 16:15:17 +0900

    Modify accounts feature

--- src/pages/SignupView.vue
+++ src/pages/SignupView.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <h1>회원가입</h1>
-    <SignupForm/>
+  <div class="overflow-auto">
+    <SignupForm class="h-100"/>
+    <div class="background d-flex">
+      <img src="@/assets/signup.png" style="width:100%; height: 100%;">
+      <!-- <img src="@/assets/signup.png" style="width:100%;"> -->
+    </div> 
   </div>
 </template>
 
@@ -16,6 +19,21 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.background {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
+  opacity: 0.5;
+  overflow: hidden;
+}
 
+img {
+  object-fit: fill;
+}
 </style>
